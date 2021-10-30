e846bee38b706d2aee910118ae60d07f3b476bb4
Travis Bowlby <travisbowlby1@gmail.com>
2021-10-30 16:23:00 -0400

    Event Bus Data Store and queue

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-Parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,19 +9,11 @@ app.use(cors());
 
 const posts = {};
 
-
-app.get('/posts', (req, res) => {
-    res.send(posts);
-
-});
-
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === 'PostCreated') {
         const { id, title } = data;
 
-        posts[id] = { id, title, comments: []};
+        posts[id] = { id, title, comments: [] };
     }
 
     if (type === 'CommentCreated') {
@@ -43,12 +36,36 @@ app.post('/events', (req, res) => {
         comment.content = content;
 
     }
+};
 
-    console.log(posts);
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
+
     res.send({});
 
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log('Listening on 4002');
+
+    try {
+        const res = await axios.get('http://localhost:4005/events');
+
+        for (let event of res.data) {
+            console.log('Processing event:', event.type);
+
+            handleEvent(event.type, event.data);
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+
 });
